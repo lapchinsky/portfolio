9142d6f5816eb783c41f72523382e13441508af3
lapchinsky <lapchinsky0308@gmail.com>
2022-04-14 12:21:44 +0300

    title style

--- src/Components/BTitle/BTitle.jsx
+++ src/Components/BTitle/BTitle.jsx
@@ -7,7 +7,7 @@ const BTitle = ({ header, strong, preview, description}) => {
             <small className={styles.title__preview}>{preview}</small>
             <h1 className={styles.title__header}>{header}</h1>
             <strong className={styles.strong}>{strong}</strong>
-            <div className={styles.title__description}>ong
+            <div className={styles.title__description}>
                 {description}
             </div>
         </div>
